2b9906f37bd6cd428db686bf1d23cfc1b78d5181
Elton Manoku <elton.manoku@fao.org>
2012-02-17 12:58:09 +0100

    Changes to the database.
    Added functions to use for lodgement report.
    The redefineCadastre requesttype is available.

--- sola.sql
+++ sola.sql
@@ -5023,6 +5023,7 @@ END;
 $BODY$
   LANGUAGE plpgsql;
 
+
 insert into system.approle_appgroup (approle_code, appgroup_id)
 SELECT r.code, 'super-group-id' FROM system.approle r 
 where r.code not in (select approle_code from system.approle_appgroup g where appgroup_id = 'super-group-id');
