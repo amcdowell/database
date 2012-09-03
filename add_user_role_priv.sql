528df5e4878216d68be87c66a1a70430648a4b4a
Elton Manoku <elton.manoku@fao.org>
2012-09-03 19:39:35 +0200

    Extract the script for priv from sola.sql
    The script to add user and privileges is extracted from sola.sql and added
    as a seperate script.

---
+++ add_user_role_priv.sql
@@ -0,0 +1,44 @@
+﻿do
+$body$
+declare
+  user_name varchar;
+  user_password varchar;
+  user_role varchar;
+  rec record;
+begin
+  user_name = 'sola_super_user';
+  user_password = 'sola';
+  user_role = 'sola_super_role';
+ --Create role
+  if (select count(*) from pg_roles where rolname = user_role)=0 then
+    execute 'create role ' || user_role;
+  end if;
+  -- Create user
+  if (select count(*) from pg_roles where rolname = user_role)=0 then
+    execute 'create user ' || user_name || ' with password ''' || user_password || ''' in role ' || user_role;
+  end if;
+
+  for rec in 
+    select distinct table_schema 
+    from information_schema.tables 
+    where table_schema not in ('pg_catalog', 'information_schema') loop
+    execute 'GRANT USAGE ON schema "'  || rec.table_schema || '" TO ' || user_role;
+  end loop;
+  -- For each table found in db, assign priv.
+  for rec in 
+    select table_schema, table_name 
+    from information_schema.tables 
+    where table_schema not in ('pg_catalog', 'information_schema') loop
+
+    if rec.table_name like '%_historic' then
+      execute 'GRANT SELECT, INSERT ON "'  || rec.table_schema || '"."' ||  rec.table_name || '" TO ' || user_role;
+    else
+      execute 'GRANT SELECT, INSERT, UPDATE, DELETE ON "'  || rec.table_schema || '"."' ||  rec.table_name || '" TO ' || user_role;
+    end if;
+  end loop;
+  for rec in select sequence_schema, sequence_name from information_schema.sequences loop
+    execute 'GRANT USAGE ON "'  || rec.sequence_schema || '"."' || rec.sequence_name || '" TO ' || user_role;
+  end loop;
+end;
+$body$;
+--select sequence_schema, sequence_name from information_schema.sequences
