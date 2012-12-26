74c8bbd3dc25ff72dcb0de510848cdccfbb01b03
unknown <RizzoM@NRC013.hq.un.fao.org>
2012-12-26 18:56:25 +0100

    added BR application-on-approve-check-public-display for checking that the duration of public notification period has been completed

--- br_target_application.sql
+++ br_target_application.sql
@@ -315,6 +315,26 @@ AND sv.status_code NOT IN (''completed'', ''cancelled'')');
 
 INSERT INTO system.br_validation(br_id, target_code, target_application_moment, severity_code, order_of_execution)
 VALUES ('application-on-approve-check-services-without-transaction', 'application', 'approve', 'critical', 330);
+----------------------------------------------------------------------------------------------------
+INSERT INTO system.br(id, technical_type_code, feedback, technical_description) 
+VALUES('application-on-approve-check-public-display', 'sql', 'The publication period must be completed.::::Il periodo di pubblica notifica deve essere completato',
+'Checks the completion of the public display period for all instances of systematic registration service related to the application');
+
+INSERT INTO system.br_definition(br_id, active_from, active_until, body) 
+VALUES('application-on-approve-check-public-display', now(), 'infinity', 
+'  SELECT (COUNT(*) = 0)  AS vl
+   FROM cadastre.land_use_type lu, cadastre.cadastre_object co, cadastre.spatial_value_area sa, administrative.ba_unit_contains_spatial_unit su, 
+   application.application_property ap, application.application aa, application.service s, source.source ss
+  WHERE sa.spatial_unit_id::text = co.id::text AND sa.type_code::text = ''officialArea''::text AND su.spatial_unit_id::text = sa.spatial_unit_id::text 
+  AND ap.ba_unit_id::text = su.ba_unit_id::text AND aa.id::text = ap.application_id::text AND s.application_id::text = aa.id::text 
+  AND s.request_type_code::text = ''systematicRegn''::text AND s.status_code::text = ''completed''::text
+   AND COALESCE(co.land_use_code, ''residential''::character varying)::text = lu.code::text
+  and ss.reference_nr = co.name_lastpart
+  and ss.reference_nr = co.name_lastpart and ss.expiration_date >= now() 
+  and s.application_id = #{id};');
+
+INSERT INTO system.br_validation(br_id, target_code, target_application_moment, severity_code, order_of_execution)
+VALUES ('application-on-approve-check-public-display', 'application', 'approve', 'critical', 600);
 
 
 ----------------------------------------------------------------------------------------------------
