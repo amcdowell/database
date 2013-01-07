26f98ad0bff7096e03548a213eacb41d013f10a9
unknown <rizzom@NRC013.hq.un.fao.org>
2013-01-07 14:49:30 +0100

    updated administrative.sys_reg_state_land for solving ticket [#272 state:fixed responsible:none]

--- sola.sql
+++ sola.sql
@@ -7223,28 +7223,6 @@ CREATE VIEW administrative.sys_reg_state_land AS SELECT (pp.name::text || ' '::t
                 END AS industrial
            FROM cadastre.cadastre_object co, cadastre.spatial_value_area sa, administrative.ba_unit_contains_spatial_unit su, application.application_property ap, application.application aa, application.service s, party.party pp, administrative.party_for_rrr pr, administrative.rrr rrr
           WHERE sa.spatial_unit_id::text = co.id::text AND sa.type_code::text = 'officialArea'::text AND su.spatial_unit_id::text = sa.spatial_unit_id::text AND ap.ba_unit_id::text = su.ba_unit_id::text AND aa.id::text = ap.application_id::text AND s.application_id::text = aa.id::text AND s.request_type_code::text = 'systematicRegn'::text AND s.status_code::text = 'completed'::text AND pp.id::text = pr.party_id::text AND pr.rrr_id::text = rrr.id::text AND rrr.ba_unit_id::text = su.ba_unit_id::text AND rrr.type_code::text = 'stateOwnership'::text
-UNION 
-         SELECT DISTINCT 'No Claimant '::text AS value, co.id, co.name_firstpart, co.name_lastpart, COALESCE(co.land_use_code, 'residential'::character varying) AS land_use_code, su.ba_unit_id, 
-                CASE
-                    WHEN COALESCE(co.land_use_code, 'residential'::character varying)::text = 'residential'::text THEN sa.size
-                    ELSE 0::numeric
-                END AS residential, 
-                CASE
-                    WHEN COALESCE(co.land_use_code, 'residential'::character varying)::text = 'agricultural'::text THEN sa.size
-                    ELSE 0::numeric
-                END AS agricultural, 
-                CASE
-                    WHEN COALESCE(co.land_use_code, 'residential'::character varying)::text = 'commercial'::text THEN sa.size
-                    ELSE 0::numeric
-                END AS commercial, 
-                CASE
-                    WHEN COALESCE(co.land_use_code, 'residential'::character varying)::text = 'industrial'::text THEN sa.size
-                    ELSE 0::numeric
-                END AS industrial
-           FROM cadastre.cadastre_object co, cadastre.spatial_value_area sa, administrative.ba_unit_contains_spatial_unit su, application.application_property ap, application.application aa, party.party pp, administrative.party_for_rrr pr, administrative.rrr rrr, application.service s
-          WHERE sa.spatial_unit_id::text = co.id::text AND sa.type_code::text = 'officialArea'::text AND su.spatial_unit_id::text = sa.spatial_unit_id::text AND ap.ba_unit_id::text = su.ba_unit_id::text AND aa.id::text = ap.application_id::text AND NOT (su.ba_unit_id::text IN ( SELECT rrr.ba_unit_id
-                   FROM administrative.rrr rrr, party.party pp, administrative.party_for_rrr pr
-                  WHERE (rrr.type_code::text = 'stateOwnership'::text OR rrr.type_code::text = 'ownership'::text OR rrr.type_code::text = 'apartment'::text OR rrr.type_code::text = 'commonOwnership'::text) AND pp.id::text = pr.party_id::text AND pr.rrr_id::text = rrr.id::text)) AND s.application_id::text = aa.id::text AND s.request_type_code::text = 'systematicRegn'::text AND s.status_code::text = 'completed'::text
   ORDER BY 1;
 
 -------View administrative.systematic_registration_listing ---------
