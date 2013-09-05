20de736e5b16d170621273b24c5b5f2325df0a50
Neil Pullar <neil.pullar@fao.org>
2013-09-05 12:54:38 +0200

    Revert change to database_create.bat

--- database_create.bat
+++ database_create.bat
@@ -1,7 +1,7 @@
 set host=localhost
 set psql_path=psql\
 set script_folder=
-set dbname=sola_nz
+set dbname=sola
 set nopass=
 set extra_options=--single-transaction --quiet -v ON_ERROR_STOP=1
 %psql_path%psql %extra_options% --host=%host% --port=5432 --username=postgres %nopass% --dbname=%dbname% --command="create database %dbname% with encoding='UTF8' template=postgistemplate connection limit=-1;"
