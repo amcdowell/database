65093761a1a03eb7ea9582564d0b311aa1f3416a
amcdowell <armcdowell@hotmail.com>
2013-09-06 16:51:13 +1200

    Lodgement Statistics Report rework [#327 responsible:none state:fixed milestone:v2.0]
    Updated report using the Samoa version as the basis. Removed code related to the
    original version of the report as it was no longer required.

--- sola_extension.sql
+++ sola_extension.sql
@@ -191,4 +191,348 @@ SELECT 'ChangePassword',  id FROM system.appgroup
 WHERE NOT EXISTS (SELECT approle_code FROM system.approle_appgroup
 				WHERE approle_code = 'ChangePassword'
 				AND   appgroup_id = id); 
+				
+				
+-- Ticket #327 Improved function for Lodgement Statistics Report 
+CREATE OR REPLACE FUNCTION application.get_work_summary(from_date DATE, to_date DATE)
+  RETURNS TABLE (
+      req_type VARCHAR(100), -- The service request type
+      req_cat VARCHAR(100), -- The service request category type
+      group_idx INT, -- Used for grouping/sorting the services by request category
+      in_progress_from INT, -- Count of the services in progress as at the from_date
+      on_requisition_from INT, -- Count of the services on requisition as at the from_date
+      lodged INT, -- Count of the services lodged during the reporting period
+      requisitioned INT, -- Count of the services requisitioned during the reporting period
+      registered INT, -- Count of the services registered during the reporting period
+      cancelled INT, -- Count of the services cancelled + the services associated to applications that have been rejected or lapsed
+      withdrawn INT, -- Count of the serices associated to an application that was withdrawn 
+      in_progress_to INT, -- Count of the services in progress as at the to_date
+      on_requisition_to INT, -- Count of the services on requisition as at the to_date
+      overdue INT, -- Count of the services exceeding their expected_completion_date at end of the reporting period
+      overdue_apps TEXT, -- The list of applications that are overdue
+      requisition_apps TEXT -- The list of applications on Requisition
+) AS
+$BODY$
+DECLARE 
+   tmp_date DATE; 
+BEGIN
+
+   IF to_date IS NULL OR from_date IS NULL THEN
+      RETURN;
+   END IF; 
+
+   -- Swap the dates so the to date is after the from date
+   IF to_date < from_date THEN 
+      tmp_date := from_date; 
+      from_date := to_date; 
+      to_date := tmp_date; 
+   END IF; 
+   
+   -- Go through to the start of the next day. 
+   to_date := to_date + 1; 
+
+   RETURN query 
+   
+      -- Identifies all services lodged during the reporting period. Uses the
+	  -- change_time instead of lodging_datetime to ensure all datetime comparisons 
+	  -- across all subqueries yield consistent results 
+      WITH service_lodged AS
+	   ( SELECT ser.id, ser.application_id, ser.request_type_code
+         FROM   application.service ser
+         WHERE  ser.change_time BETWEEN from_date AND to_date
+		 AND    ser.rowversion = 1
+		 UNION
+         SELECT ser_hist.id, ser_hist.application_id, ser_hist.request_type_code
+         FROM   application.service_historic ser_hist
+         WHERE  ser_hist.change_time BETWEEN from_date AND to_date
+		 AND    ser_hist.rowversion = 1),
+		 
+      -- Identifies all services cancelled during the reporting period. 	  
+	  service_cancelled AS 
+        (SELECT ser.id, ser.application_id, ser.request_type_code
+         FROM   application.service ser
+         WHERE  ser.change_time BETWEEN from_date AND to_date
+		 AND    ser.status_code = 'cancelled'
+     -- Verify that the service actually changed status 
+         AND  NOT EXISTS (SELECT ser_hist.status_code 
+                          FROM application.service_historic ser_hist
+                          WHERE ser_hist.id = ser.id
+                          AND  (ser.rowversion - 1) = ser_hist.rowversion
+                          AND  ser.status_code = ser_hist.status_code )
+	 -- Check the history data for cancelled services as applications returned
+	 -- from requisition can cause the cancelled service record to be updated. 
+		UNION
+		SELECT ser.id, ser.application_id, ser.request_type_code
+         FROM   application.service_historic ser
+         WHERE  ser.change_time BETWEEN from_date AND to_date
+		 AND    ser.status_code = 'cancelled'
+     -- Verify that the service actually changed status. 
+         AND  NOT EXISTS (SELECT ser_hist.status_code 
+                          FROM application.service_historic ser_hist
+                          WHERE ser_hist.id = ser.id
+                          AND  (ser.rowversion - 1) = ser_hist.rowversion
+                          AND  ser.status_code = ser_hist.status_code )),
+		
+      -- All services in progress at the end of the reporting period		
+      service_in_progress AS (  
+         SELECT ser.id, ser.application_id, ser.request_type_code, ser.expected_completion_date
+	 FROM application.service ser
+	 WHERE ser.change_time <= to_date
+	 AND ser.status_code IN ('pending', 'lodged')
+      UNION
+	 SELECT ser_hist.id, ser_hist.application_id, ser_hist.request_type_code, 
+	        ser_hist.expected_completion_date
+	 FROM  application.service_historic ser_hist,
+	       application.service ser
+	 WHERE ser_hist.change_time <= to_date
+	 AND   ser.id = ser_hist.id
+	 -- Filter out any services that have not been changed since the to_date as these
+	 -- would have been picked up in the first select if they were still active
+	 AND   ser.change_time > to_date
+	 AND   ser_hist.status_code IN ('pending', 'lodged')
+	 AND   ser_hist.rowversion = (SELECT MAX(ser_hist2.rowversion)
+				      FROM  application.service_historic ser_hist2
+				      WHERE ser_hist.id = ser_hist2.id
+				      AND   ser_hist2.change_time <= to_date )),
+	
+    -- All services in progress at the start of the reporting period	
+	service_in_progress_from AS ( 
+     SELECT ser.id, ser.application_id, ser.request_type_code, ser.expected_completion_date
+	 FROM application.service ser
+	 WHERE ser.change_time <= from_date
+	 AND ser.status_code IN ('pending', 'lodged')
+     UNION
+	 SELECT ser_hist.id, ser_hist.application_id, ser_hist.request_type_code, 
+	        ser_hist.expected_completion_date
+	 FROM  application.service_historic ser_hist,
+	       application.service ser
+	 WHERE ser_hist.change_time <= from_date
+	 AND   ser.id = ser_hist.id
+	 -- Filter out any services that have not been changed since the from_date as these
+	 -- would have been picked up in the first select if they were still active
+	 AND   ser.change_time > from_date
+	 AND   ser_hist.status_code IN ('pending', 'lodged')
+	 AND   ser_hist.rowversion = (SELECT MAX(ser_hist2.rowversion)
+				      FROM  application.service_historic ser_hist2
+				      WHERE ser_hist.id = ser_hist2.id
+				      AND   ser_hist2.change_time <= from_date )),
+				      
+    app_changed AS ( -- All applications that changed status during the reporting period
+	                 -- If the application changed status more than once, it will be listed
+					 -- multiple times
+         SELECT app.id, 
+	 -- Flag if the application was withdrawn
+	 app.status_code,
+	 CASE app.action_code WHEN 'withdrawn' THEN TRUE ELSE FALSE END AS withdrawn
+	 FROM   application.application app
+	 WHERE  app.change_time BETWEEN from_date AND to_date
+	 -- Verify that the application actually changed status during the reporting period
+	 -- rather than just being updated
+	 AND  NOT EXISTS (SELECT app_hist.status_code 
+			  FROM application.application_historic app_hist
+			  WHERE app_hist.id = app.id
+			  AND  (app.rowversion - 1) = app_hist.rowversion
+			  AND  app.status_code = app_hist.status_code )
+      UNION  
+	 SELECT app_hist.id, 
+	 app_hist.status_code,
+	 CASE app_hist.action_code WHEN 'withdrawn' THEN TRUE ELSE FALSE END AS withdrawn
+	 FROM  application.application_historic app_hist
+	 WHERE app_hist.change_time BETWEEN from_date AND to_date
+	 -- Verify that the application actually changed status during the reporting period
+	 -- rather than just being updated
+	 AND  NOT EXISTS (SELECT app_hist2.status_code 
+			  FROM application.application_historic app_hist2
+			  WHERE app_hist.id = app_hist2.id
+			  AND  (app_hist.rowversion - 1) = app_hist2.rowversion
+			  AND  app_hist.status_code = app_hist2.status_code )), 
+                          
+     app_in_progress AS ( -- All applications in progress at the end of the reporting period
+	 SELECT app.id, app.status_code, app.expected_completion_date, app.nr
+	 FROM application.application app
+	 WHERE app.change_time <= to_date
+	 AND app.status_code IN ('lodged', 'requisitioned')
+	 UNION
+	 SELECT app_hist.id, app_hist.status_code, app_hist.expected_completion_date, app_hist.nr
+	 FROM  application.application_historic app_hist, 
+	       application.application app
+	 WHERE app_hist.change_time <= to_date
+	 AND   app.id = app_hist.id
+	 -- Filter out any applications that have not been changed since the to_date as these
+	 -- would have been picked up in the first select if they were still active
+	 AND   app.change_time > to_date
+	 AND   app_hist.status_code IN ('lodged', 'requisitioned')
+	 AND   app_hist.rowversion = (SELECT MAX(app_hist2.rowversion)
+				      FROM  application.application_historic app_hist2
+				      WHERE app_hist.id = app_hist2.id
+				      AND   app_hist2.change_time <= to_date)),
+					  
+	app_in_progress_from AS ( -- All applications in progress at the start of the reporting period
+	 SELECT app.id, app.status_code, app.expected_completion_date, app.nr
+	 FROM application.application app
+	 WHERE app.change_time <= from_date
+	 AND app.status_code IN ('lodged', 'requisitioned')
+	 UNION
+	 SELECT app_hist.id, app_hist.status_code, app_hist.expected_completion_date, app_hist.nr
+	 FROM  application.application_historic app_hist,
+	       application.application app
+	 WHERE app_hist.change_time <= from_date
+	 AND   app.id = app_hist.id
+	-- Filter out any applications that have not been changed since the from_date as these
+	-- would have been picked up in the first select if they were still active
+	 AND   app.change_time > from_date
+	 AND   app_hist.status_code IN ('lodged', 'requisitioned')
+	 AND   app_hist.rowversion = (SELECT MAX(app_hist2.rowversion)
+				      FROM  application.application_historic app_hist2
+				      WHERE app_hist.id = app_hist2.id
+				      AND   app_hist2.change_time <= from_date))
+   -- MAIN QUERY                         
+   SELECT get_translation(req.display_value, null) AS req_type,
+	  CASE req.request_category_code 
+	     WHEN 'registrationServices' THEN get_translation(cat.display_value, null)
+	     WHEN 'cadastralServices' THEN get_translation(cat.display_value, null)
+	     ELSE 'Information Services'  END AS req_cat,
+	     
+	  CASE req.request_category_code 
+	     WHEN 'registrationServices' THEN 1
+             WHEN 'cadastralServices' THEN 2
+	     ELSE 3 END AS group_idx,
+		 
+	  -- Count of the pending and lodged services associated with
+	  -- lodged applications at the start of the reporting period
+         (SELECT COUNT(s.id) FROM service_in_progress_from s, app_in_progress_from a
+          WHERE s.application_id = a.id
+          AND   a.status_code = 'lodged'
+	  AND request_type_code = req.code)::INT AS in_progress_from,
+
+	  -- Count of the services associated with requisitioned 
+	  -- applications at the end of the reporting period
+         (SELECT COUNT(s.id) FROM service_in_progress_from s, app_in_progress_from a
+	  WHERE s.application_id = a.id
+          AND   a.status_code = 'requisitioned'
+	  AND s.request_type_code = req.code)::INT AS on_requisition_from,
+	     
+	  -- Count the services lodged during the reporting period.
+	 (SELECT COUNT(s.id) FROM service_lodged s
+	  WHERE s.request_type_code = req.code)::INT AS lodged,
+	  
+      -- Count the applications that were requisitioned during the
+	  -- reporting period. All of the services on the application
+ 	  -- are requisitioned unless they are cancelled. Use the
+	  -- current set of services on the application, but ensure
+	  -- the services where lodged before the end of the reporting
+	  -- period and that they were not cancelled during the 
+	  -- reporting period. 
+	 (SELECT COUNT(s.id) FROM app_changed a, application.service s
+          WHERE s.application_id = a.id
+	  AND   a.status_code = 'requisitioned'
+	  AND   s.lodging_datetime < to_date
+	  AND   NOT EXISTS (SELECT can.id FROM service_cancelled can
+                        WHERE s.id = can.id)	  
+          AND   s.request_type_code = req.code)::INT AS requisitioned, 
+          
+	  -- Count the services on applications approved/completed 
+	  -- during the reporting period. Note that services cannot be
+	  -- changed after the application is approved, so checking the
+	  -- current state of the services is valid. 
+         (SELECT COUNT(s.id) FROM app_changed a, application.service s
+	  WHERE s.application_id = a.id
+	  AND   a.status_code = 'approved'
+	  AND   s.status_code = 'completed'
+	  AND   s.request_type_code = req.code)::INT AS registered,
+	  
+	  -- Count of the services associated with applications 
+	  -- that have been lapsed or rejected + the count of 
+	  -- services cancelled during the reporting period. Note that
+      -- once annulled changes to the services are not possible so
+      -- checking the current state of the services is valid.
+      (SELECT COUNT(tmp.id) FROM  	  
+        (SELECT s.id FROM app_changed a, application.service s
+		  WHERE s.application_id = a.id
+		  AND   a.status_code = 'annulled'
+		  AND   a.withdrawn = FALSE
+		  AND   s.request_type_code = req.code
+          UNION		  
+		  SELECT s.id FROM app_changed a, service_cancelled s
+		  WHERE s.application_id = a.id
+		  AND   a.status_code != 'annulled'
+		  AND   s.request_type_code = req.code) AS tmp)::INT AS cancelled, 
+	  
+	  -- Count of the services associated with applications
+	  -- that have been withdrawn during the reporting period
+	  -- Note that once annulled changes to the services are
+      -- not possible so checking the current state of the services is valid. 
+         (SELECT COUNT(s.id) FROM app_changed a, application.service s
+	  WHERE s.application_id = a.id
+	  AND   a.status_code = 'annulled'
+	  AND   a.withdrawn = TRUE
+	  AND   s.status_code != 'cancelled'
+	  AND   s.request_type_code = req.code)::INT AS withdrawn,
+
+	  -- Count of the pending and lodged services associated with
+	  -- lodged applications at the end of the reporting period
+         (SELECT COUNT(s.id) FROM service_in_progress s, app_in_progress a
+          WHERE s.application_id = a.id
+          AND   a.status_code = 'lodged'
+	  AND request_type_code = req.code)::INT AS in_progress_to,
+
+	  -- Count of the services associated with requisitioned 
+	  -- applications at the end of the reporting period
+         (SELECT COUNT(s.id) FROM service_in_progress s, app_in_progress a
+	  WHERE s.application_id = a.id
+          AND   a.status_code = 'requisitioned'
+	  AND s.request_type_code = req.code)::INT AS on_requisition_to,
+
+	  -- Count of the services that have exceeded thier expected
+	  -- completion date and are overdue. Only counts the service 
+	  -- as overdue if both the application and the service are overdue. 
+         (SELECT COUNT(s.id) FROM service_in_progress s, app_in_progress a
+          WHERE s.application_id = a.id
+          AND   a.status_code = 'lodged'              
+	  AND   a.expected_completion_date < to_date
+	  AND   s.expected_completion_date < to_date
+	  AND   s.request_type_code = req.code)::INT AS overdue,  
+
+	  -- The list of overdue applications 
+	 (SELECT string_agg(a.nr, ', ') FROM app_in_progress a
+          WHERE a.status_code = 'lodged' 
+          AND   a.expected_completion_date < to_date
+          AND   EXISTS (SELECT s.application_id FROM service_in_progress s
+                        WHERE s.application_id = a.id
+                        AND   s.expected_completion_date < to_date
+                        AND   s.request_type_code = req.code)) AS overdue_apps,   
+
+	  -- The list of applications on Requisition
+	 (SELECT string_agg(a.nr, ', ') FROM app_in_progress a
+          WHERE a.status_code = 'requisitioned' 
+          AND   EXISTS (SELECT s.application_id FROM service_in_progress s
+                        WHERE s.application_id = a.id
+                        AND   s.request_type_code = req.code)) AS requisition_apps 						
+   FROM  application.request_type req, 
+	 application.request_category_type cat
+   WHERE req.status = 'c'
+   AND   cat.code = req.request_category_code					 
+   ORDER BY group_idx, req_type;
+	
+   END; $BODY$
+   LANGUAGE plpgsql VOLATILE;
+
+
+COMMENT ON FUNCTION application.get_work_summary(DATE,DATE)
+IS 'Returns a summary of the services processed for a specified reporting period';
+
+-- Create indexes to improve performance of the Lodgement Report
+DROP INDEX IF EXISTS application.service_historic_id_idx;
+DROP INDEX IF EXISTS application.application_historic_id_idx;
+
+ CREATE INDEX service_historic_id_idx
+  ON application.service_historic
+  USING btree
+  (id COLLATE pg_catalog."default");
+
+CREATE INDEX application_historic_id_idx
+  ON application.application_historic
+  USING btree
+  (id COLLATE pg_catalog."default");
+
   
