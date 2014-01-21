3d01058e2fc8815f8b6adb2b1a8cdd98db6b6449
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-01-21 13:54:50 +0100

    #388 Business Rule - False Failure - Convert to Digital Title service completion Business rule modified to exclude the pending ba_unit record from the BR search

--- sola_extension.sql
+++ sola_extension.sql
@@ -2143,3 +2143,16 @@ $BODY$
 
 COMMENT ON FUNCTION system.consolidation_consolidate(character varying) IS 'It moves the records from the temporary consolidation schema to the main tables.';
 
+-- #388 Business Rule - False Failure - Convert to Digital Title service completion 
+UPDATE "system".br_definition
+   SET body='SELECT coalesce(not rrr.is_primary, true) as vl
+FROM application.service s inner join application.application_property ap on s.application_id = ap.application_id
+  INNER JOIN administrative.ba_unit ba ON (ap.name_firstpart, ap.name_lastpart) = (ba.name_firstpart, ba.name_lastpart)
+  LEFT JOIN administrative.rrr ON rrr.ba_unit_id = ba.id
+WHERE s.id = #{id} 
+AND ba.status_code != ''pending''
+order by 1 desc
+limit 1'
+ WHERE br_id= 'service-check-no-previous-digital-title-service';
+
+
